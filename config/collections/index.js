59a16d5f09adf5a2499f3546add5f650f2c9d639
William Whitaker <william@williamwhitaker.dev>
2023-05-19 23:24:06 -0400

    did the same for projects what I did for posts

--- config/collections/index.js
+++ config/collections/index.js
@@ -1,12 +1,11 @@
 /** Returns all project descriptions as a collection **/
-const sortByNumber = require('./utils');
+const sortByNumber = require("./utils");
 
-
-const getAllProjects = collection => {
-  const projects = collection.getFilteredByTag('projects').sort(sortByNumber);
-  return projects;
+const getAllProjects = (collection) => {
+  const projects = collection.getFilteredByTag("projects").sort(sortByNumber);
+  return `projects: ${projects}`;
 };
 
 module.exports = {
-  getAllProjects
+  getAllProjects,
 };
