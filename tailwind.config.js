c05e2a6a27cf5637de143e2db666cff68318c745
William Whitaker <william@williamwhitaker.dev>
2023-05-21 19:40:06 -0400

    adjusting fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,11 @@ module.exports = {
   content: ["./src/**/*.{njk,md}", "./_site/**/*.html"],
   presets: [require("./src/assets/design-tokens/typography.js")],
   theme: {
-    fontFamily: {
-      sans: ["Fredoka One", "sans-serif"],
-      serif: ["Cormorant Garamond", "serif"],
-      mono: ["Fira Code", "monospace"],
+    extend: {
+      fontFamily: {
+        display: ['"Fredoka One"', "sans-serif"],
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
