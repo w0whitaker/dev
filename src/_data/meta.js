cd9ab4e904d6655042ee4ee452ce7cd3d0e16354
William Whitaker <william@williamwhitaker.dev>
2023-05-20 20:26:16 -0400

    some layout stuff

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,8 +1,7 @@
 module.exports = {
   url: process.env.URL || "http://localhost:8080",
   siteName: "William Whitaker",
-  siteDescription:
-    "front-end developer",
+  siteDescription: "a front-end developer",
   authorName: "William Whitaker",
   twitterUsername: "w0whitaker", // no `@`
 };
