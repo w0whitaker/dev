9e395bafe6580503a1ccc0ed6448ed7ca6553161
William Whitaker <william@williamwhitaker.dev>
2023-05-21 14:38:55 -0400

    sets font temporarily

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{njk,md}", "./_site/**/*.html"],
   presets: [require("./src/assets/design-tokens/typography.js")],
   theme: {
+    fontFamily: {
+      sans: ["Fredoka One", "sans-serif"],
+      serif: ["Cormorant Garamond", "serif"],
+      mono: ["Fira Code", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
