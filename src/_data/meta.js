6faeae355c0bd5f4ca44a9d20e6a90dbc8f2e06a
William Whitaker <william@williamwhitaker.dev>
2023-08-11 18:35:18 -0400

    bunch of renaming and tidying

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,7 +1,8 @@
+/** @format */
+
 module.exports = {
-  url: process.env.URL || "http://localhost:8080",
-  siteName: "William Whitaker",
-  siteDescription: "a front-end developer",
-  authorName: "William Whitaker",
-  twitterUsername: "w0whitaker", // no `@`
+  url: process.env.URL || 'http://localhost:8080',
+  siteName: 'William Whitaker',
+  siteDescription: 'among other things, a front-end developer',
+  authorName: 'William Whitaker',
 };
--- src/projects/projects.11tydata.js
+++ src/projects/projects.11tydata.js
@@ -1,3 +1,5 @@
+/** @format */
+
 module.exports = {
   eleventyComputed: {
     pageTitle: (data) => data.title,
