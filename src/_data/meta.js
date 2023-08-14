29b32d73eaf755d8a5e603c3c88196affda27afc
William Whitaker <william@williamwhitaker.dev>
2023-08-14 14:14:45 -0400

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -3,6 +3,6 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
   siteName: 'William Whitaker',
-  siteDescription: 'among other things, a front-end developer',
+  siteDescription: 'a front-end developer',
   authorName: 'William Whitaker',
 };
