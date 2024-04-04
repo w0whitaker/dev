3fbab3cc2f748ac5e95ff5b6d7b98c6c56a64e6a
William Whitaker <william@williamwhitaker.dev>
2024-04-04 17:10:47 -0400

    removes cssmin filter

--- config/filters/index.js
+++ config/filters/index.js
@@ -1,6 +1,5 @@
 const isoDate = require("./iso-date/iso-date.js");
 const humanDate = require("./human-date/human-date.js");
 const md = require("./markdown/markdown.js");
-const cssmin = require("./cssmin/cssmin.js");
 
-module.exports = { isoDate, humanDate, md, cssmin };
+module.exports = { isoDate, humanDate, md };
