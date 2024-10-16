520ac6811c4293e6a736eee009945b168beaecea
William Whitaker <william@williamwhitaker.dev>
2024-10-16 04:50:48 -0400

    add metadata.js
    cherry picked from liquid branch

---
+++ src/_data/metadata.js
@@ -0,0 +1,10 @@
+export default {
+	title: 'william dot dev',
+	url: 'https://williamwhitaker.dev',
+	language: 'en',
+	description:
+		"I'm an artist, art handler, and front-end developer (looking for my first gig).",
+	author: {
+		name: 'William Whitaker',
+	},
+};
