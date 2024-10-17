324ad9615b6362700995feece75c0695ce8042ec
William Whitaker <william@williamwhitaker.dev>
2024-10-17 16:45:23 -0400

    update navigation

--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -3,4 +3,24 @@ export default [
 		text: 'home',
 		url: '/',
 	},
+	{
+		text: 'projects',
+		url: '/projects',
+	},
+	{
+		text: 'posts',
+		url: '/posts',
+	},
+	{
+		text: 'contact',
+		url: '/contact',
+	},
+	{
+		text: 'links',
+		url: '/links',
+	},
+	{
+		text: 'design system',
+		url: '/design-system',
+	},
 ];
