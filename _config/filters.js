ef9c2b472c79885f436cd18a57a44b9b2b40a03b
William Whitaker <william@williamwhitaker.dev>
2024-12-19 15:35:33 -0500

    refactor: remove unused filter

--- _config/filters.js
+++ _config/filters.js
@@ -39,26 +39,4 @@ export default function (eleventyConfig) {
 	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
 		return (tags || []).filter((tag) => ["all", "posts"].indexOf(tag) === -1);
 	});
-
-	eleventyConfig.addFilter("isPublished", function isPublished(item) {
-		if ("data" in item) {
-			// Check for 'draft: true' in the post's frontmatter and exit if found.
-			if ("draft" in item.data && item.data.draft === true) {
-				return false;
-			}
-			// Check for 'published: false' in the post's frontmatter and exit if found.
-			if ("published" in item.data && item.data.published === false) {
-				return false;
-			}
-			// Check if the post has the tag 'ignore' and exit if so.
-			if ("tags" in item.data && item.data.tags.includes("ignore")) {
-				return false;
-			}
-			// Check if post's date is in the future and exit if so.
-			if ("date" in item.data && epoch(item.data.date) > nowEpoch) {
-				return false;
-			}
-		}
-		return !!item.url;
-	});
 }
