b3d1f7ea312e69747f9366e1343dcb235514bec1
William Whitaker <william@williamwhitaker.dev>
2025-03-21 16:49:29 -0400

    feat: add links page

--- _config/shortcodes/longUrl.js
+++ _config/shortcodes/longUrl.js
@@ -1,28 +1,11 @@
 /**
  * Add <wbr> to long urls
- *
+ * https://gitlab.com/reubenlillie/reubenlillie.com/-/blob/main/_includes/filters/format-url.js
  * @param {string} url A raw URL to format
  * @return {string} A formatted URL
  */
-
-// export default (url) => {
-
-// 	const formatted = doubleSlash
-// 		.map((str) =>
-// 			str
-// 				.replace(/(?<colon>:)/giu, "$1<wbr>")
-// 				.replace(/?<before>[/~.,\-_?#%])/giu, "<wbr>$1")
-// 				.replace(/?<both>[=&])/giu, "<wbr>$1<wbr>")
-// 		)
-// 		.join("//<wbr>");
-
-// 	return formatted;
-// };
-//
-
 export function longUrl(url) {
 	const doubleSlash = url.split("//");
-	console.log(doubleSlash);
 	const formatted = doubleSlash
 		.map((str) =>
 			str
